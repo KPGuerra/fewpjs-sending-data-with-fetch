d46e522ee02e7a96b160cd33b66bfce40f0c8ec9
KPGuerra <KPGuerra96@gmail.com>
2020-11-19 22:15:09 -0500

    commit tree 343f86bb79ac232b6b546c8cf92105cf959f293c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+
+
 // Add your code here
+function submitData (name, email) {
+    return fetch ('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',
+        "Accept": "application/json"},
+        body: JSON.stringify({name, email})
+    })
+    .then(response => response.json())
+    .then(newUser => {
+        document.body.append(newUser["id"])
+    })
+    .catch( error => {
+        document.body.append(error.message)
+    })
+}
